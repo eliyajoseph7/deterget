c84fe62a66fc4b05414bf09b7f3fe8873eae17ca
eliyajoseph7 <exaveryeliya20@gmail.com>
2024-03-25 10:40:30 +0300

    new commit

---
+++ app/Livewire/Pages/Dashboard/Components/BestSellingProduct.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Pages\Dashboard\Components;
+
+use Livewire\Component;
+
+class BestSellingProduct extends Component
+{
+    public function render()
+    {
+        return view('livewire.pages.dashboard.components.best-selling-product');
+    }
+}
