caf47ae2cda5c5167fe858575f5fe430e9ccbcf3
eliyajoseph7 <exaveryeliya20@gmail.com>
2024-03-28 09:04:02 +0300

    modified

--- app/Livewire/Pages/Report/Sale/Sale.php
+++ app/Livewire/Pages/Report/Sale/Sale.php
@@ -3,8 +3,10 @@
 namespace App\Livewire\Pages\Report\Sale;
 
 use App\Exports\Reports\Sale\SaleReport;
+use App\Models\Remain;
 use App\Models\Sale as ModelsSale;
 use DateTime;
+use DateTimeZone;
 use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 use Livewire\WithPagination;
@@ -62,12 +64,36 @@ class Sale extends Component
     public function fetchReport()
     {
 
-        $data = ModelsSale::search($this->search)->leftjoin('remains', 'remains.product_id', 'sales.product_id')
+        // $data = ModelsSale::search($this->search)
+        //     ->leftjoin('remains', 'remains.product_id', 'sales.product_id')
+        //     ->join('users', 'users.id', 'sales.seller_id')
+        //     ->select('users.name', 'sales.product_id', 'sales.date', DB::raw('SUM(sales.quantity) as sold'), DB::raw('SUM(remains.quantity) as remained'))
+        //     ->groupBy('sales.date', 'sales.product_id', 'users.name')
+        //     ->whereMonth('sales.date', $this->date->format('m'))->whereYear('sales.date', $this->date->format('Y'))
+        //     ->orderBy($this->sortBy, $this->sortDir)->get()->groupBy('name')->all();
+
+        $data = ModelsSale::search($this->search)
             ->join('users', 'users.id', 'sales.seller_id')
-            ->select('users.name', 'sales.product_id', 'sales.date', DB::raw('SUM(sales.quantity) as sold'), DB::raw('SUM(remains.quantity) as remained'))
-            ->groupBy('sales.date', 'sales.product_id', 'users.name')
+            ->select('users.name', 'seller_id', 'sales.product_id', 'sales.date', DB::raw('SUM(sales.quantity) as sold'))
+            ->groupBy('sales.date', 'sales.product_id', 'users.name', 'seller_id')->with('product')
             ->whereMonth('sales.date', $this->date->format('m'))->whereYear('sales.date', $this->date->format('Y'))
-            ->orderBy($this->sortBy, $this->sortDir)->get()->groupBy('name')->all();
+            ->orderBy($this->sortBy, $this->sortDir)->get();
+
+        $result = collect([]);
+
+        foreach ($data as $dt) {
+            $remain = Remain::where('product_id', $dt->product_id)->where(DB::raw("(DATE_FORMAT(date,'%Y-%m-%d'))"), '=', $dt->date)->where('user_id', $dt->seller_id)->sum('quantity');
+            $result->push([
+                "name" => $dt->name,
+                "product_id" => $dt->product_id,
+                "product" => $dt->product,
+                "date" => $dt->date,
+                "sold" => $dt->sold,
+                "remain" => $remain
+            ]);
+        }
+
+        $this->data = $result->groupBy('name');
 
         $diff = strtotime(now()) - strtotime(now() . ' -' . $this->previousCount . ' month');
         if ($diff == 0) {
@@ -75,8 +101,6 @@ class Sale extends Component
         } else {
             $this->toNext = True;
         }
-
-        $this->data = $data;
     }
 
 
@@ -88,7 +112,8 @@ class Sale extends Component
 
     public function mount()
     {
-        $this->date = now();
+        $date = new DateTime("now", new DateTimeZone('Africa/Dar_es_Salaam'));
+        $this->date = $date;
         // $this->total_days = cal_days_in_month(CAL_GREGORIAN, $this->date->format('m'), $this->date->format('Y'));
         $this->fetchReport();
     }
