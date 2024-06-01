933b557277d0c309d778c1874c2f6aad1ea072bf
eliyajoseph7 <exaveryeliya20@gmail.com>
2024-06-01 11:59:37 +0300

    modified

--- app/Helpers/DashboardReports/BestSellingProductReport.php
+++ app/Helpers/DashboardReports/BestSellingProductReport.php
@@ -12,7 +12,7 @@ class BestSellingProductReport {
         $chart = Sale::join('products', 'products.id', 'sales.product_id')->select('products.name as name', DB::raw('COUNT(sales.id) as y') )
                     ->groupBy('products.name')->orderByDesc('y')->limit(6)->get();
 
-        if($chart) {
+        if($chart->count() > 0) {
             $chart[0]['sliced'] = true;
             $chart[0]['selected'] = true;
         }
