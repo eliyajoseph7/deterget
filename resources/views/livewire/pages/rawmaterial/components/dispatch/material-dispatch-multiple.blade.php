35213cc66aee73524f0609c332da8e399677be3d
eliyajoseph7 <exaveryeliya20@gmail.com>
2024-06-03 08:41:31 +0300

    modified

--- resources/views/livewire/pages/rawmaterial/components/dispatch/material-dispatch-multiple.blade.php
+++ resources/views/livewire/pages/rawmaterial/components/dispatch/material-dispatch-multiple.blade.php
@@ -71,7 +71,7 @@
                             </div>
                             <div class="col-span-3">
                                 <label for="purpose"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Invoice
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Purpose
                                     <span class="text-red-500">*</span></label>
                                 <textarea wire:model.lazy="fields.{{ $index }}.purpose" rows="2"
                                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full px-2.5 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
