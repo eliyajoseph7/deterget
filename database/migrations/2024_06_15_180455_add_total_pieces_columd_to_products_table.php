aad45242879025367f2c53bab03a66c0c9a37da4
eliyajoseph7 <exaveryeliya20@gmail.com>
2024-06-18 12:23:05 +0300

    modified

---
+++ database/migrations/2024_06_15_180455_add_total_pieces_columd_to_products_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->integer('total_pieces')->after('selling_price')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('total_pieces');
+        });
+    }
+};
